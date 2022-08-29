5d648b22711d472a64452a3a37f77aedce1446cd
Wojciech Malarski <w.malarski@gmail.com>
2022-08-29 21:11:31 +0200

    improving styles

--- src/server/coverArt.ts
+++ src/server/coverArt.ts
@@ -1,3 +1,19 @@
+import type { Prisma } from "@prisma/client";
+
 export type CoverKind = "small" | "large";
 
-export type CoversField = Record<CoverKind, string>;
+export type CoversField = Partial<Record<CoverKind, string[]>>;
+
+export const jsonToCoversField = (json?: Prisma.JsonValue): CoversField => {
+  if (!json) {
+    return {};
+  }
+  return json as CoversField;
+};
+
+export const frontCover = (sid?: string | null): string | null => {
+  if (!sid) {
+    return null;
+  }
+  return `http://coverartarchive.org/release/${sid}/front-250.jpg`;
+};
