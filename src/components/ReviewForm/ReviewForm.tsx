853f73daea5a89b42aedb987f2207dc92a787a60
Wojciech Malarski <w.malarski@gmail.com>
2022-09-01 22:07:44 +0200

    album card review form added

--- src/components/ReviewForm/ReviewForm.tsx
+++ src/components/ReviewForm/ReviewForm.tsx
@@ -1,66 +1,92 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import clsx from "clsx";
 import { createSignal, JSX, Show } from "solid-js";
 
-export const ReviewForm = (): JSX.Element => {
+type Props = {
+  albumId: string;
+  onSuccess: () => void;
+  onCancel: () => void;
+};
+
+export const ReviewForm = (props: Props): JSX.Element => {
   const [text, setText] = createSignal("");
   const [rate, setRate] = createSignal(5);
 
-  const [status, setStatus] = createSignal({
-    error: "",
-    isLoading: false,
-    isSuccess: false,
-  });
+  const [isLoading, setIsLoading] = createSignal(false);
+  const [error, setError] = createSignal("");
 
   const handleSubmit = async () => {
-    setStatus({ error: "", isLoading: true, isSuccess: false });
+    setError("");
+    setIsLoading(true);
 
-    const response = await fetch("/api/review");
+    const body = JSON.stringify({
+      albumId: props.albumId,
+      rate: rate(),
+      text: text(),
+    });
+    const response = await fetch("/api/review", { body });
     const result = await response.json();
 
-    setStatus(
-      result.error
-        ? { error: result.error, isLoading: false, isSuccess: false }
-        : { error: "", isLoading: false, isSuccess: true }
-    );
+    setError(result.error || "");
+    setIsLoading(false);
 
     if (!result.error) {
-      //
+      props.onSuccess();
     }
   };
 
   return (
-    <div class="w-full">
-      <div class="pb-2">
-        To log in, or register. Use the form below to get a magic link to your
-        email.
-      </div>
-      <div>
+    <div class="w-full flex flex-col gap-2">
+      <div class="flex gap-2 w-full">
+        <label for="review" class="label label-text">
+          Review
+        </label>
         <input
-          class="input"
-          disabled={status().isLoading}
-          id="email"
+          class="input input-sm  flex-grow"
+          disabled={isLoading()}
+          id="review"
           onChange={(event) => setText(event.currentTarget.value)}
-          placeholder="Email"
+          placeholder="Review"
           type="text"
           value={text()}
         />
-        <Show when={status().error} keyed>
-          {(error) => <div class="text-sm text-red-400">{error}</div>}
-        </Show>
-        <Show when={status().isSuccess}>
-          <div class="text-sm text-green-600">
-            An email should arrive in your inbox shortly
-          </div>
-        </Show>
       </div>
-      <div class="pt-3 relative">
+      <div class="flex gap-2  w-full">
+        <label for="rate" class="label label-text">
+          Rate
+        </label>
+        <input
+          class="input input-sm flex-grow"
+          disabled={isLoading()}
+          id="rate"
+          max={10}
+          min={0}
+          onChange={(event) => setRate(event.currentTarget.valueAsNumber)}
+          placeholder="Rate"
+          step={0.1}
+          type="number"
+          value={rate()}
+        />
+      </div>
+      <Show when={error()} keyed>
+        {(error) => <div class="text-sm text-red-400">{error}</div>}
+      </Show>
+      <div class="flex justify-end w-full gap-2">
         <button
-          disabled={status().isLoading}
-          class={clsx("btn btn-primary", { loading: status().isLoading })}
+          disabled={isLoading()}
+          class={clsx("btn btn-ghost", { loading: isLoading() })}
+          type="button"
+          onClick={() => props.onCancel()}
+        >
+          Cancel
+        </button>
+        <button
+          disabled={isLoading()}
+          class={clsx("btn btn-primary", { loading: isLoading() })}
           type="button"
           onClick={handleSubmit}
         >
-          Send magic link to your email!
+          Save
         </button>
       </div>
     </div>
