8e9c852bf8ad93e7b005b1cec55f4be9994155e6
Wojciech Malarski <w.malarski@gmail.com>
2022-09-01 20:36:27 +0200

    paths file added

---
+++ src/utils/paths.ts
@@ -0,0 +1,35 @@
+export const paths = {
+  album: ({ id }: { id: string }): string => {
+    return `/album/${id}`;
+  },
+  albumRelease: ({ sid }: { sid: string }): string => {
+    return `http://coverartarchive.org/release/${sid}`;
+  },
+  index: ({ page, query }: { page?: number; query?: string } = {}): string => {
+    const pageParam = page ? { page: `${page}` } : {};
+    const queryParam = query ? { query } : {};
+    const params = { ...pageParam, ...queryParam };
+    return `/?${new URLSearchParams(params)}`;
+  },
+  login: "/login",
+  logout: "/logout",
+  reviews: ({
+    lower,
+    page,
+    query,
+    upper,
+  }: {
+    page?: number;
+    query?: string;
+    lower?: number;
+    upper?: number;
+  } = {}): string => {
+    const params = {
+      ...(page ? { page: `${page}` } : {}),
+      ...(query ? { query } : {}),
+      ...(lower ? { lower: `${lower}` } : {}),
+      ...(upper ? { upper: `${upper}` } : {}),
+    };
+    return `/reviews?${new URLSearchParams(params)}`;
+  },
+};
