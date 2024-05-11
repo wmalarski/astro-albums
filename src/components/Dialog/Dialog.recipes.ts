0ffed7ef857b784dc66e185f58ef285d62e65035
Wojciech Malarski <w.malarski@gmail.com>
2024-05-11 11:57:17 +0200

    adds dialog component

---
+++ src/components/Dialog/Dialog.recipes.ts
@@ -0,0 +1,7 @@
+import { twCva } from "@components/utils/twCva";
+
+export const dialogContainerClass = twCva("modal");
+
+export const dialogContentClass = twCva("modal-box");
+
+export const dialogActionsClass = twCva("modal-actions");
---
+++ src/components/Dialog/Dialog.tsx
@@ -0,0 +1,69 @@
+import { splitProps, type Component } from "solid-js";
+import {
+  dialogActionsClass,
+  dialogContainerClass,
+  dialogContentClass,
+} from "./Dialog.recipes";
+import type { ComponentVariantProps } from "@components/utils/twCva";
+import { buttonClass } from "@components/Button/Button.recipes";
+import { buttonSplitProps } from "@components/Button/Button";
+
+export type DialogTriggerProps = ComponentVariantProps<
+  "button",
+  typeof buttonClass
+> & { dialogId: string };
+
+export const DialogTrigger: Component<DialogTriggerProps> = (props) => {
+  const [variants, propsNoStyle] = splitProps(props, buttonSplitProps);
+  const [split, rest] = splitProps(propsNoStyle, ["dialogId"]);
+
+  const onClick: DialogTriggerProps["onClick"] = () => {
+    const elementId = `dialog#${split.dialogId}`;
+    const element = document.querySelector<HTMLDialogElement>(elementId);
+    element?.showModal();
+  };
+
+  return <button {...rest} onClick={onClick} class={buttonClass(variants)} />;
+};
+
+export type DialogCloseProps = ComponentVariantProps<
+  "button",
+  typeof buttonClass
+>;
+
+export const DialogClose: Component<DialogCloseProps> = (props) => {
+  const [split, rest] = splitProps(props, buttonSplitProps);
+
+  return (
+    <form method="dialog">
+      <button {...rest} class={buttonClass(split)} />
+    </form>
+  );
+};
+
+export type DialogContainerProps = ComponentVariantProps<
+  "dialog",
+  typeof dialogContainerClass
+> & { id: string };
+
+export const DialogContainer: Component<DialogContainerProps> = (props) => {
+  return <dialog {...props} class={dialogContainerClass()} />;
+};
+
+export type DialogContentProps = ComponentVariantProps<
+  "div",
+  typeof dialogContentClass
+>;
+
+export const DialogContent: Component<DialogContentProps> = (props) => {
+  return <div {...props} class={dialogContentClass()} />;
+};
+
+export type DialogActionsProps = ComponentVariantProps<
+  "div",
+  typeof dialogActionsClass
+>;
+
+export const DialogActions: Component<DialogActionsProps> = (props) => {
+  return <div {...props} class={dialogActionsClass()} />;
+};
