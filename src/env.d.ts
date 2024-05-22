86cfab7ae15b33e36a112384ffc6c5ff7b1b9575
Wojciech Malarski <w.malarski@gmail.com>
2024-05-22 20:25:46 +0200

    fixes eslint errors

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,4 @@
 /// <reference path="../.astro/actions.d.ts" />
-/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference path="../.astro/db-types.d.ts" />
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
