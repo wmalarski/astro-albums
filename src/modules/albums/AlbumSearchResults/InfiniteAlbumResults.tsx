bc29cd2e0ede30f079b827371f7828469cec421c
Wojciech Malarski <w.malarski@gmail.com>
2024-05-24 19:27:36 +0200

    adds loading search albums

---
+++ src/modules/albums/AlbumSearchResults/InfiniteAlbumResults.tsx
@@ -0,0 +1,60 @@
+import { Button } from "@components/Button/Button";
+import { CardGrid } from "@modules/common/CardGrid";
+import { actions } from "astro:actions";
+import { For, createSignal, type Component, type ParentProps } from "solid-js";
+import { createStore, produce } from "solid-js/store";
+import { AlbumCard } from "../AlbumCard/AlbumCard";
+
+type InfiniteAlbumResultsProps = ParentProps<{
+  query: string;
+}>;
+
+type ResultsStore = {
+  page: number;
+  results: Awaited<ReturnType<typeof actions.findAlbumsByQuery>>;
+};
+
+export const InfiniteAlbumResults: Component<InfiniteAlbumResultsProps> = (
+  props,
+) => {
+  const [isLoading, setIsLoading] = createSignal(false);
+
+  const [albums, setAlbums] = createStore<ResultsStore>({
+    page: 0,
+    results: [],
+  });
+
+  const onClick = async () => {
+    setIsLoading(true);
+
+    try {
+      const newAlbums = await actions.findAlbumsByQuery({
+        page: albums.page,
+        query: props.query,
+      });
+
+      setAlbums(
+        produce((state) => {
+          state.page += 1;
+          state.results.push(...newAlbums);
+        }),
+      );
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div class="flex flex-col gap-4">
+      <CardGrid>
+        {props.children}
+        <For each={albums.results}>
+          {(entry) => <AlbumCard album={entry.Album} artist={entry.Artist} />}
+        </For>
+      </CardGrid>
+      <Button onClick={onClick} isLoading={isLoading()}>
+        Load more
+      </Button>
+    </div>
+  );
+};
---
+++ src/modules/common/CardGrid.tsx
@@ -0,0 +1,9 @@
+import type { Component, ParentProps } from "solid-js";
+
+export const CardGrid: Component<ParentProps> = (props) => {
+  return (
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+      {props.children}
+    </div>
+  );
+};
