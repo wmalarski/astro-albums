42195ae489d5ed003b023b2bfa8e8034ff35a15e
Wojciech Malarski <w.malarski@gmail.com>
2024-08-08 22:23:38 +0200

    starts infinite random albums web component

--- src/modules/common/CardGrid.tsx
+++ src/modules/common/CardGrid.tsx
@@ -2,7 +2,10 @@ import type { Component, ParentProps } from "solid-js";
 
 export const CardGrid: Component<ParentProps> = (props) => {
   return (
-    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+    <div
+      {...props}
+      class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4"
+    >
       {props.children}
     </div>
   );
